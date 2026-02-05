cfeaca94001629ef6680cabef3b2b17b15ca82c2
PotatoCP <kenzieradityatirtarahardja18@gmail.com>
2026-02-05 14:50:28 +0800

    Use CMake instead of MakeFile
    Signed-off-by: PotatoCP <kenzieradityatirtarahardja18@gmail.com>

---
+++ CMakeFiles/termid-x.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/termid-x.dir/link.d"
+  "CMakeFiles/termid-x.dir/src/account.cpp.o"
+  "CMakeFiles/termid-x.dir/src/account.cpp.o.d"
+  "CMakeFiles/termid-x.dir/src/engine.cpp.o"
+  "CMakeFiles/termid-x.dir/src/engine.cpp.o.d"
+  "CMakeFiles/termid-x.dir/src/entity.cpp.o"
+  "CMakeFiles/termid-x.dir/src/entity.cpp.o.d"
+  "CMakeFiles/termid-x.dir/src/main.cpp.o"
+  "CMakeFiles/termid-x.dir/src/main.cpp.o.d"
+  "CMakeFiles/termid-x.dir/src/stock.cpp.o"
+  "CMakeFiles/termid-x.dir/src/stock.cpp.o.d"
+  "termid-x"
+  "termid-x.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/termid-x.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/termid-x.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for termid-x.
+# This may be replaced when dependencies are built.
