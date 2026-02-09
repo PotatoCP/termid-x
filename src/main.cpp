2adbc6a21c8e107c001bd04e27093d98e8933a86
PotatoCP <kenzieradityatirtarahardja18@gmail.com>
2026-02-09 15:52:33 +0800

    Benchmarking the order
    Signed-off-by: PotatoCP <kenzieradityatirtarahardja18@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -19,25 +19,43 @@ int main() {
 
         std::random_device rd{};
         std::mt19937 gen{rd()};
-        std::normal_distribution price_generator{400.0, 10.0};
+        std::normal_distribution price_generator{400.0, 200.0};
 
         auto random_price = [&price_generator, &gen]{ return std::lround(price_generator(gen)); };
 
-        for(int i = 0; i < 200; ++i) {
+        double total_time_ms = 0.0;
+        int total_orders = 100'000;
+
+        struct OrderTest {
+            std::string symbol;
+            int user_id;
+            int quantity;
+            int price;
+            bool is_bid;
+        };
+
+        std::vector<OrderTest> orders;
+
+        for(int i = 0; i < total_orders; ++i) {
             int ask_or_bid = gen() % 2;
             int quantity = gen() % 1000 + 1;
             int price = random_price();
-            if(ask_or_bid == 0) {
-                std::cout << "Bidding: " << quantity << " units at $" << price << "\n";
-                market_engine.place_bid(1, "TUNA", quantity, price);
+            orders.push_back({"TUNA", ask_or_bid + 1, quantity, price, ask_or_bid == 0});
+        }
+
+        for(const auto& order : orders) {
+            auto start_time = std::chrono::high_resolution_clock::now();
+            if(order.is_bid) {
+                market_engine.place_bid(order.user_id, order.symbol, order.quantity, order.price);
             } else {
-                std::cout << "Asking: " << quantity << " units at $" << price << "\n";
-                market_engine.place_ask(2, "TUNA", quantity, price);
+                market_engine.place_ask(order.user_id, order.symbol, order.quantity, order.price);
             }
-            std::cout << "Price of TUNA: " << market_engine.get_entity_price("TUNA") << "\n";
+             auto end_time = std::chrono::high_resolution_clock::now();
+            total_time_ms += std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count();
         }
 
         std::cout << "Final Price of TUNA: " << market_engine.get_entity_price("TUNA") << "\n";
+        std::cout << "Average time per order: " << total_time_ms / total_orders << " ns\n";
 
     });
     engine_thread.join();
